92708faa2c70c373297916b96671d96100dcf951
ggntju <ggntju@163.com>
2020-05-27 21:55:35 -0500

    add cuda header and source file

---
+++ src/CUDACalculator.cu
@@ -0,0 +1,10 @@
+//
+// Created by Guannan Guo on 5/27/20.
+//
+
+#include CUDACalculator.h
+
+__global__ void cuda_calculate_greyscale(const cudaTextureObject_t tex_img, const int pixels) {
+
+}
+
---
+++ src/CUDACalculator.h
@@ -0,0 +1,24 @@
+//
+// Created by Guannan Guo on 5/27/20.
+//
+
+#ifndef GREYSCALECUDA_CUDACALCULATOR_H
+#define GREYSCALECUDA_CUDACALCULATOR_H
+#pragma once
+
+#include <cstdint>
+
+#include "cuda_runtime.h"
+
+#ifdef __INTELLISENSE__
+#define asm(x)
+#define min(x) 0
+#define fmaf(x) 0
+#include "device_launch_parameters.h"
+#define __CUDACC__
+#include "device_functions.h"
+#undef __CUDACC__
+#endif
+
+void cuda_calculate_greyscale(const cudaTextureObject_t tex_img, const int pixels);
+#endif //GREYSCALECUDA_CUDACALCULATOR_H
