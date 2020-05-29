38047c945aa5e89a812e3ac2389329977cb84395
ggntju <ggntju@163.com>
2020-05-29 00:39:46 -0500

    read uchar array

--- src/GreyScaleCalculator.cpp
+++ src/GreyScaleCalculator.cpp
@@ -77,9 +77,8 @@ double GreyScaleCalculator::CUDA_greyscale() {
     Rect ROI(this->origin[0], this->origin[1], this->dimension[0], this->dimension[1]);
     Mat roi_domain(total_domain, ROI);
     int arraySize = roi_domain.rows * roi_domain.cols * 3;
-    long int* roi_pointer = static_cast<long int*>(roi_domain.data);
     for(int i = 0; i < arraySize; i++) {
-        cout << roi_pointer[i] << endl;
+        cout << roi_domain.data[i] << endl;
         cin.get();
     }
 //    cudaMalloc(&roi_pointer, arraySize);
