d80815a1b7718b53539a3b0dadd145bb17a68964
ggntju <ggntju@163.com>
2020-05-29 17:25:55 -0500

    fix something

--- src/GPUWarmer.cpp
+++ src/GPUWarmer.cpp
@@ -1,7 +1,8 @@
 //
 // Created by Guannan Guo on 5/29/20.
 //
-
+#include <iostream>
+#include <opencv2/opencv.hpp>
 #include "GPUWarmer.h"
 #include "CUDARGB2Y.h"
 #include "cuda_runtime.h"
@@ -34,6 +35,19 @@ void GPUWarmer::warmUp() {
     uint8_t* d_img;
     cudaMalloc(&d_img, 3*image.rows*image.cols);
     cudaMemcpy(d_img, image.data, 3*image.rows*image.cols, cudaMemcpyHostToDevice);
+    struct cudaResourceDesc resDesc;
+    memset(&resDesc, 0, sizeof(resDesc));
+    resDesc.resType = cudaResourceTypeLinear;
+    resDesc.res.linear.devPtr = d_img;
+    resDesc.res.linear.desc.f = cudaChannelFormatKindUnsigned;
+    resDesc.res.linear.desc.x = 8;
+    resDesc.res.linear.sizeInBytes = 3 * image.rows * image.cols;
+    struct cudaTextureDesc texDesc;
+    memset(&texDesc, 0, sizeof(texDesc));
+    texDesc.addressMode[0] = texDesc.addressMode[1] = texDesc.addressMode[2] = texDesc.addressMode[3] = cudaAddressModeBorder;
+    texDesc.filterMode = cudaFilterModePoint;
+    texDesc.readMode = cudaReadModeElementType;
+    texDesc.normalizedCoords = 0;
     cudaTextureObject_t tex_img = 0;
     cudaCreateTextureObject(&tex_img, &resDesc, &texDesc, nullptr);
     // allocating space for new image
