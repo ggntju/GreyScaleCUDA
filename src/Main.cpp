8c349a10c9a85eb8ca2e3c7227cfa0e2c552725f
ggntju <ggntju@163.com>
2020-05-29 17:45:31 -0500

    change roi

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,8 +18,10 @@ int main( int argc, char** argv )
 	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     // Mat origins = (Mat_<double>(2,2) << 20.0, 75.0, 190.0, 340.0);
     // Mat dimensions = (Mat_<double>(2,2) << 290.0, 75.0, 90.0, 50.0);
-    Mat origins = (Mat_<double>(1,2) << 190.0, 340.0);
-    Mat dimensions = (Mat_<double>(1,2) << 90.0, 50.0);
+    // Mat origins = (Mat_<double>(1,2) << 190.0, 340.0);
+    // Mat dimensions = (Mat_<double>(1,2) << 90.0, 50.0);
+    Mat origins = (Mat_<double>(1,2) << 20.0, 75.0);
+    Mat dimensions = (Mat_<double>(1,2) << 290.0, 75.0);
 	GreyScaleAnalysisControler controler(origins, dimensions, "../ImageData", "bmp");
 	//controler.print2Console();
 	Mat histData = controler.get_histogram();
