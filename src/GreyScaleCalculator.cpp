0077b6c5966396c00d2ca610e56550834f242b73
ggntju <ggntju@163.com>
2020-05-28 23:48:54 -0500

    small fix

--- src/GreyScaleCalculator.cpp
+++ src/GreyScaleCalculator.cpp
@@ -77,7 +77,7 @@ double GreyScaleCalculator::CUDA_greyscale() {
     Rect ROI(this->origin[0], this->origin[1], this->dimension[0], this->dimension[1]);
     Mat roi_domain(total_domain, ROI);
     int arraySize = roi_domain.rows * roi_domain.cols * 3;
-    long int* roi_pointer = roi_domain.data;
+    long int* roi_pointer = (long int*)roi_domain.data;
 //    cudaMalloc(&roi_pointer, arraySize);
 //    cudaMemcpy(roi_pointer, roi_domain.data, arraySize, cudaMemcpyHostToDevice);
     // call cuda function
